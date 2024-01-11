a280feb0c25a9a2471725bf808d3aed8ec4a9814
Briton Otieno <ojwangbriton@gmail.com>
2024-01-11 11:18:40 +0300

    Update references

--- db/migrate/20240111080053_create_comments.rb
+++ db/migrate/20240111080053_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.references :user_id, null: false, foreign_key: { to_table: :users }
-      t.references :post_id, null: false, foreign_key: { to_table: :users }
+      t.references :user, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: { to_table: :users }
       t.text :text
 
       t.timestamps
--- db/migrate/20240111080457_create_likes.rb
+++ db/migrate/20240111080457_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.1]
   def change
     create_table :likes do |t|
-      t.references :user_id, null: false, foreign_key: { to_table: :users }
-      t.references :post_id, null: false, foreign_key: { to_table: :users }
+      t.references :user, null: false, foreign_key: { to_table: :users }
+      t.references :post, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
