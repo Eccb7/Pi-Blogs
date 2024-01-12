c36e77939b93d954bab66e9322d3fac2f634d955
Briton Otieno <ojwangbriton@gmail.com>
2024-01-12 10:22:52 +0300

    Update schema for the comments migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_11_080457) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_12_071240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,9 +53,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_080457) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
   add_foreign_key "comments", "users", column: "post_id"
-  add_foreign_key "comments", "users", column: "user_id"
+  add_foreign_key "likes", "users"
   add_foreign_key "likes", "users", column: "post_id"
-  add_foreign_key "likes", "users", column: "user_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
