b25a85516ba3c6c7b5ad99fe9f0b5102ccfbd6d1
Briton Otieno <ojwangbriton@gmail.com>
2024-01-12 14:27:52 +0300

    Add validations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  validates :text, presence: true
+
+  attribute :text, :text
+  attribute :user_id, :integer
+  attribute :post_id, :integer
+
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   after_create :increment_post_comments_counter
 
