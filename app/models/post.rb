0c6160884da5b3ee7c5862458007853eea2a3ae2
Briton Otieno <ojwangbriton@gmail.com>
2024-01-12 10:20:56 +0300

    Add associations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :author_id
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+
+  after_create :increment_user_posts_counter
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def increment_user_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
