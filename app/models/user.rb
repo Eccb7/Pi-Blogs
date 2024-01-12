ba09aa3402977824e9e0957a7474308bda08037f
Briton Otieno <ojwangbriton@gmail.com>
2024-01-12 14:29:37 +0300

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   attribute :name, :string
   attribute :photo, :string
   attribute :bio, :text
+  attribute :posts_counter, :integer
 
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
