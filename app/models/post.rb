4ae3a93ba78b66a0e7de1fd36bd929b41a2e0cac
Briton Otieno <ojwangbriton@gmail.com>
2024-01-12 14:28:58 +0300

    Add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  attribute :title, :string
+  attribute :comments_counter, :integer
+  attribute :likes_counter, :integer
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
   has_many :likes
