039eb5f96fc602d51e0559b841c0663a6316ddf9
Briton Otieno <ojwangbriton@gmail.com>
2024-01-12 14:34:33 +0300

    Add spec configurations

--- Gemfile
+++ Gemfile
@@ -65,3 +65,7 @@ group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
 end
+
+group :test, :development do
+  gem 'rspec-rails', '~> 6.0.0'
+end
--- db/migrate/20240112071411_rename_user_id_id_to_user_id_in_likes.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserIdIdToUserIdInLikes < ActiveRecord::Migration[7.1]
-  def change
-    rename_column :likes, :user_id_id, :user_id
-  end
-end
