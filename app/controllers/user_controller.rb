3712957c8599bfc2c75040275e5cb3af213c6648
Briton Otieno <ojwangbriton@gmail.com>
2024-01-18 04:32:35 +0300

    Add user controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,16 @@
+class UserController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      # Handle the case where the user is not found, e.g., redirect to an error page
+      redirect_to root_path, alert: 'User not found'
+    else
+      @posts = @user.posts
+    end
+  end
+end
