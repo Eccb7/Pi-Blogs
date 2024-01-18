43dbac96abd41de3380308d26d0bcb40201ee464
Briton Otieno <ojwangbriton@gmail.com>
2024-01-18 20:34:26 +0300

    Add the post request spec

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -2,6 +2,44 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'renders the index template' do
+      user = create(:user)
+      post = create(:post, author: user)
+
+      get user_posts_path(user)
+
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:index)
+    end
+
+    it 'assigns all posts to @posts' do
+      user = create(:user)
+      post = create(:post, author: user)
+
+      get user_posts_path(user)
+
+      expect(assigns(:posts)).to eq([post])
+    end
+  end
+
+  describe 'GET /show' do
+    it 'renders the show template' do
+      user = create(:user)
+      post = create(:post, author: user)
+
+      get user_post_path(user, post)
+
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:show)
+    end
+
+    it 'assigns the requested post to @post' do
+      user = create(:user)
+      post = create(:post, author: user)
+
+      get user_post_path(user, post)
+
+      expect(assigns(:post)).to eq(post)
+    end
   end
 end
