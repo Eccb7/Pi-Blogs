0e405d7f5c3d66f9ec25c81b61c1c5e891d73e70
Briton Otieno <ojwangbriton@gmail.com>
2024-01-18 06:17:49 +0300

    Update the post controller

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,10 +1,17 @@
 class PostController < ApplicationController
+  before_action :set_user
+
   def index
-    @user = User.find(params[:id])
-    @user.posts
+    @posts = @user.posts
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = @user.posts.find(params[:id])
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
