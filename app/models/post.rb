5dcddd859861129b048e2b32e43d1b513b762620
Briton Otieno <ojwangbriton@gmail.com>
2024-01-26 05:44:42 +0300

    Update counter attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   attribute :title, :string
-  attribute :comments_counter, :integer
-  attribute :likes_counter, :integer
+  attribute :comments_counter, :integer, default: 0
+  attribute :likes_counter, :integer, default: 0
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments
