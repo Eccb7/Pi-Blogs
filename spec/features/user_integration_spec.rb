d2e274fcf7f756e88d17ab804a7da6b3061539a0
Briton Otieno <ojwangbriton@gmail.com>
2024-01-31 11:50:49 +0300

    Fix rubocop error

--- spec/features/user_integration_spec.rb
+++ spec/features/user_integration_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature 'User Integration', type: :feature do
     visit users_path
 
     expect(page).to have_content(user.name)
-    expect(page).to have_selector("img.img-thumbnail")
+    expect(page).to have_selector('img.img-thumbnail')
     expect(page).to have_content("Number of posts: #{user.posts_counter}")
 
     click_link user.name
@@ -17,7 +17,7 @@ RSpec.feature 'User Integration', type: :feature do
   scenario 'User show page' do
     visit user_path(user)
 
-    expect(page).to have_selector("img.img-thumbnail")
+    expect(page).to have_selector('img.img-thumbnail')
     expect(page).to have_content(user.name)
     expect(page).to have_content("Number of posts: #{user.posts_counter}")
     expect(page).to have_content(user.bio)
@@ -30,39 +30,38 @@ RSpec.feature 'User Integration', type: :feature do
       expect(page).to have_link('See all Posts', href: user_posts_path(user))
       click_link user.posts.first.title
       expect(current_path).to eq(user_post_path(user, user.posts.first))
-    # else
-    #   # Handle the case when the user has no posts
-    #   # For example, you can add an expectation or a message indicating that there are no posts.
-    #   expect(page).to have_text("This user has no posts.")
+      # else
+      #   # Handle the case when the user has no posts
+      #   # For example, you can add an expectation or a message indicating that there are no posts.
+      #   expect(page).to have_text("This user has no posts.")
     end
-    
-    end
-    scenario 'User post index page' do
-      visit user_posts_path(user)
-      expect(page).to have_selector("img.img-thumbnail")
-      expect(page).to have_content(user.name)
-      expect(page).to have_content("Number of posts: #{user.posts_counter}")
-      user.recent_posts.each do |post|
-        expect(page).to have_content(post.title)
-        expect(page).to have_content(post.text.truncate(50))
-        expect(page).to have_content("Comments: #{post.comments_counter}")
-        expect(page).to have_content("Likes: #{post.likes_counter}")
-        click_link post.title
-        expect(current_path).to eq(user_post_path(user, post))
-        visit user_posts_path(user) # Go back to user post index
-      end
-    end
-    scenario 'Post show page' do
-      post = create(:post, author: user)
-      visit user_post_path(user, post)
+  end
+  scenario 'User post index page' do
+    visit user_posts_path(user)
+    expect(page).to have_selector('img.img-thumbnail')
+    expect(page).to have_content(user.name)
+    expect(page).to have_content("Number of posts: #{user.posts_counter}")
+    user.recent_posts.each do |post|
       expect(page).to have_content(post.title)
-      expect(page).to have_content("by #{post.author.name}")
+      expect(page).to have_content(post.text.truncate(50))
       expect(page).to have_content("Comments: #{post.comments_counter}")
       expect(page).to have_content("Likes: #{post.likes_counter}")
-      expect(page).to have_content(post.text)
-      post.comments.each do |comment|
-        expect(page).to have_content(comment.user.name)
-        expect(page).to have_content(comment.text)
-      end
+      click_link post.title
+      expect(current_path).to eq(user_post_path(user, post))
+      visit user_posts_path(user) # Go back to user post index
     end
+  end
+  scenario 'Post show page' do
+    post = create(:post, author: user)
+    visit user_post_path(user, post)
+    expect(page).to have_content(post.title)
+    expect(page).to have_content("by #{post.author.name}")
+    expect(page).to have_content("Comments: #{post.comments_counter}")
+    expect(page).to have_content("Likes: #{post.likes_counter}")
+    expect(page).to have_content(post.text)
+    post.comments.each do |comment|
+      expect(page).to have_content(comment.user.name)
+      expect(page).to have_content(comment.text)
+    end
+  end
 end
