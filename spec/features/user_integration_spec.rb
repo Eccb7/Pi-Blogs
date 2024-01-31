2c37108cb67a3f39250bc947fdf79f85fe59d1e5
Briton Otieno <ojwangbriton@gmail.com>
2024-01-31 11:47:23 +0300

    Update the user integration specs

--- spec/features/user_integration_spec.rb
+++ spec/features/user_integration_spec.rb
@@ -35,33 +35,34 @@ RSpec.feature 'User Integration', type: :feature do
     #   # For example, you can add an expectation or a message indicating that there are no posts.
     #   expect(page).to have_text("This user has no posts.")
     end
-  end
-  scenario 'User post index page' do
-    visit user_posts_path(user)
-    expect(page).to have_selector("img.img-thumbnail")
-    expect(page).to have_content(user.name)
-    expect(page).to have_content("Number of posts: #{user.posts_counter}")
-    user.recent_posts.each do |post|
+    
+    end
+    scenario 'User post index page' do
+      visit user_posts_path(user)
+      expect(page).to have_selector("img.img-thumbnail")
+      expect(page).to have_content(user.name)
+      expect(page).to have_content("Number of posts: #{user.posts_counter}")
+      user.recent_posts.each do |post|
+        expect(page).to have_content(post.title)
+        expect(page).to have_content(post.text.truncate(50))
+        expect(page).to have_content("Comments: #{post.comments_counter}")
+        expect(page).to have_content("Likes: #{post.likes_counter}")
+        click_link post.title
+        expect(current_path).to eq(user_post_path(user, post))
+        visit user_posts_path(user) # Go back to user post index
+      end
+    end
+    scenario 'Post show page' do
+      post = create(:post, author: user)
+      visit user_post_path(user, post)
       expect(page).to have_content(post.title)
-      expect(page).to have_content(post.text.truncate(50))
+      expect(page).to have_content("by #{post.author.name}")
       expect(page).to have_content("Comments: #{post.comments_counter}")
       expect(page).to have_content("Likes: #{post.likes_counter}")
-      click_link post.title
-      expect(current_path).to eq(user_post_path(user, post))
-      visit user_posts_path(user) # Go back to user post index
+      expect(page).to have_content(post.text)
+      post.comments.each do |comment|
+        expect(page).to have_content(comment.user.name)
+        expect(page).to have_content(comment.text)
+      end
     end
-  end
-  scenario 'Post show page' do
-    post = create(:post, author: user)
-    visit user_post_path(user, post)
-    expect(page).to have_content(post.title)
-    expect(page).to have_content("by #{post.author.name}")
-    expect(page).to have_content("Comments: #{post.comments_counter}")
-    expect(page).to have_content("Likes: #{post.likes_counter}")
-    expect(page).to have_content(post.text)
-    post.comments.each do |comment|
-      expect(page).to have_content(comment.user.name)
-      expect(page).to have_content(comment.text)
-    end
-  end
 end
