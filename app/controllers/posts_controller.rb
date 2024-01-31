d77f900bd856c9b17629fb1462a50c75c3bb4616
Briton Otieno <ojwangbriton@gmail.com>
2024-01-31 23:29:41 +0300

    Fic the N+1 problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_user, only: %i[index show new create like]
 
   def index
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
