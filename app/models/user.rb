8dd4299db5e15a87ad3401a29a38f2fb835bf8e3
Briton Otieno <ojwangbriton@gmail.com>
2024-02-01 11:37:24 +0300

    Add the devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true
+  validates :surname, presence: true
+  validates :email, uniqueness: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   attribute :name, :string
+  attribute :surname, :string
+  attribute :email, :string
   attribute :photo, :string
   attribute :bio, :text
   attribute :posts_counter, :integer
