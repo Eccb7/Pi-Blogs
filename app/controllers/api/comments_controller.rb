1cf7ebb1b234f798d84b29b6fa805932c86568b2
Briton Otieno <ojwangbriton@gmail.com>
2024-02-02 13:08:08 +0300

    Add error message to commets api if not found

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -23,6 +23,8 @@ module Api
     def set_user_and_post
       @user = User.find(params[:user_id])
       @post = @user.posts.find(params[:post_id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'Post not found' }, status: :not_found
     end
 
     def comment_params
