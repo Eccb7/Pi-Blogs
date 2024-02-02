ccc0df6a4535e7bdd0d56f37d579f77ae0fc826a
Briton Otieno <ojwangbriton@gmail.com>
2024-02-02 12:46:48 +0300

    Add comments api

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,32 @@
+module Api
+  class CommentsController < ApplicationController
+    before_action :set_user_and_post
+
+    def index
+      @comments = @post.comments
+      render json: @comments
+    end
+
+    def create
+      @comment = @post.comments.new(comment_params)
+      @comment.user = @user
+
+      if @comment.save
+        render json: @comment, status: :created
+      else
+        render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def set_user_and_post
+      @user = User.find(params[:user_id])
+      @post = @user.posts.find(params[:post_id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+  end
+end
