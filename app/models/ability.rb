0d6d48108df8f3a428a35a55e8e1413c01e8330a
Briton Otieno <ojwangbriton@gmail.com>
2024-02-02 05:59:38 +0300

    Add the Cancancan ability.rb

---
+++ app/models/ability.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    can :read, :all
+
+    if user.persisted?
+      can :manage, Post
+      can :like, Post
+      can :destroy, Post, author_id: user.id
+      can :destroy, Comment, user_id: user.id
+    end
+
+    if user.admin?
+      can :manage, :all
+    end
+  end
+end
